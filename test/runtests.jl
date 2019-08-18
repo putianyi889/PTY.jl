1843ebc4d518c5a2c71318871da951b7a29f0cf3
putianyi889 <44583944+putianyi889@users.noreply.github.com>
2019-08-18 21:55:20 +0100

    add test

---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using PTY,Test
+
+@testset "FunArray" begin
+    B=FunArray{Float64,3}(x->x[1]+x[2]*x[3],(2,3,4))
+    B[2,2,end]
+    B[:,1:end,3]
+    B[:,:,:]
+
+    show(B)
+    show(FunMatrix(Float64,x->x[1]^x[2],3,3))
+    show(FunVector(Float64,sqrt,10))
+end
