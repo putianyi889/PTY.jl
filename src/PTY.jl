c590fb8fa8c9ccb0a3fdc52685a6736ec0c2df3a
putianyi889 <44583944+putianyi889@users.noreply.github.com>
2019-08-18 19:15:58 +0100

    fix bug

--- src/PTY.jl
+++ src/PTY.jl
@@ -7,11 +7,13 @@ struct FunArray{T,n} <: AbstractArray{T,n}
     sz::Tuple{TT} where TT <: Integer
 end
 Base.size(A::FunArray)=A.sz
-Base.getindex(A::FunArray{T,N},I::Vararg{Int, N}) where {T,N}=A.fun(I)
+Base.getindex(A::FunArray{T,N},I::Vararg{Int,N}) where {T,N}=A.fun(I)
 
 const FunVector{T}=FunArray{T,1}
 const FunMatrix{T}=FunArray{T,2}
 
 FunArray(T::Type,fun::Function,sz...)=FunArray{T,length(sz)}(fun,sz)
+FunVector(T::Type,fun::Function,sz:Int)=FunArray{T,1}(fun,(sz))
+FunMatrix(T::Type,fun::Function,sz1::Int,sz2::Int)=FunArray{T,2}(fun,(sz1,sz2))
 
 end # module
