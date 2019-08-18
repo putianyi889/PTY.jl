527a31075bc8227d914f5d7e0b8597a687454a0a
putianyi889 <44583944+putianyi889@users.noreply.github.com>
2019-08-18 22:14:39 +0100

    fb

--- src/PTY.jl
+++ src/PTY.jl
@@ -13,7 +13,7 @@ const FunVector{T}=FunArray{T,1}
 const FunMatrix{T}=FunArray{T,2}
 
 FunArray(T::Type,fun::Function,sz...)=FunArray{T,length(sz)}(fun,sz)
-FunVector(T::Type,fun::Function,sz::Int)=FunArray{T,1}(fun,sz,)
+FunVector(T::Type,fun::Function,sz::Int)=FunArray{T,1}(fun,(sz,))
 FunMatrix(T::Type,fun::Function,sz1::Int,sz2::Int)=FunArray{T,2}(fun,(sz1,sz2))
 
 end # module
