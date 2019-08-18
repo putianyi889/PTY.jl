9698053f8c08069b8203d810563af5e98cab9b20
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2019-08-18 17:05:37 +0100

    Update PTY.jl

--- src/PTY.jl
+++ src/PTY.jl
@@ -3,11 +3,12 @@ module PTY
 export FunArray,FunVector,FunMatrix
 
 struct FunArray{T,n} <: AbstractArray{T,n}
+    fun::Function
     sz::Array{TT,1} where TT <: Integer
-	fun::Function
 end
 Base.size(A::FunArray)=A.sz
 Base.getindex(A::FunArray,ind...)=A.fun(ind)
+Base.getindex(A::FunArray,ind)=A.fun(ind)
 
 const FunVector{T}=FunArray{T,1}
 const FunMatrix{T}=FunArray{T,2}
