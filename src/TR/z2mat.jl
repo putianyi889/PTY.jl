3e5c9e7c971c90af2d91c9d51e32c2240533e1e0
Tianyi Pu <912396513@qq.com>
2023-06-21 23:55:20 +0100

    reorganise codes
    change definitions and names of `nthbit` and `setnthbit`
    add `flipbit`, `xorbits`, `exchangebits`.

--- src/TR/z2mat.jl
+++ src/TR/z2mat.jl
@@ -3,189 +3,14 @@ import Base: *, +, &, |, ⊻, ⊼, ⊽, ~, \
 import Base: UndefInitializer, Dims
 import LinearAlgebra: dot, AdjOrTrans, mul!, rank, det, checksquare, ldiv!, SingularException
 
-"""
-    bit2type(n::Integer) :: DataType
-
-Returns minimal integer type that contains `n` bits
-
-# Examples
-```jldoctest
-julia> TR.bit2type(5)
-UInt8
-
-julia> TR.bit2type(9)
-UInt16
-
-julia> TR.bit2type(32)
-UInt32
-
-julia> TR.bit2type(33)
-UInt64
-
-julia> TR.bit2type(100)
-BigInt
-```
-"""
-function bit2type(n::Integer)
-    if n <= 8
-        UInt8
-    elseif n <= 16
-        UInt16
-    elseif n <= 32
-        UInt32
-    elseif n <= 64
-        UInt64
-    elseif isinf(n)
-        throw(ArgumentError("doesn't support infinite bits"))
-    else
-        BigInt
-    end
-end
-
-type2bit(::Type{T}) where T<:Integer = sizeof(T) << 3
-
-"""
-    nthbit(x::Integer, n::Integer)
-
-Get the `n`-th last bit of `x`. See also [`setnthbit`](@ref)
-
-# Example
-```jldoctest
-julia> [TR.nthbit(0b10110, n) for n in 1:5]
-5-element Vector{Bool}:
- 0
- 1
- 1
- 0
- 1
-```
-"""
-nthbit(x::Integer, n::Integer) = isodd(x >> (n-1))
-
-"""
-    setnthbit(x::Integer, y::Bool, n::Integer)
-
-Set the `n`-th last bit of `x` to `y`. See also [`nthbit`](@ref).
-
-# Example
-```jldoctest
-julia> string(TR.setnthbit(0b10110, true, 1), base = 2)
-"10111"
-
-julia> string(TR.setnthbit(0b10110, false, 1), base = 2)
-"10110"
-```
-"""
-setnthbit(x::Integer, y::Bool, n::Integer) = ifelse(y, x | 1<<(n-1), x & ~(1<<(n-1)))
-
-function rowmatmulvec(x::AbstractVector{<:Integer}, y::Integer)
-    ret = zero(y)
-    for u in x
-        ret <<= 1
-        ret += fuse(u, y)
-    end
-    ret
-end
-
-function colmatmulvec(x::AbstractVector{<:Integer}, y::Integer)
-    ret = zero(y)
-    for u in x
-        ret = ifelse(isodd(y), ret ⊻ u, ret)
-        y >>= 1
-    end
-    ret
-end
-
-function matrank!(x::AbstractVector{T}) where T<:Integer
-    r = 0
-    for i in lastindex(x):-1:1
-        k = trailing_zeros(x[i])
-        if k < type2bit(T)
-            r += true
-            for j in i-1:-1:1
-                if isodd(x[j] >> k)
-                    x[j] ⊻= x[i]
-                end
-            end
-        end
-    end
-    r
-end
-
-function matdet!(x::AbstractVector{T}) where T<:Integer
-    for i in lastindex(x):-1:1
-        k = trailing_zeros(x[i])
-        if k == type2bit(T)
-            return false
-        end
-        for j in i-1:-1:1
-            if isodd(x[j] >> k)
-                x[j] ⊻= x[i]
-            end
-        end
-    end
-    return true
-end
-
-function rowmatldivrowmat!(z::AbstractVector{U}, x::AbstractVector{T}, y::AbstractVector{U}) where {T,U}
-    p = zeros(T, length(x))
-    for i in eachindex(x)
-        if iszero(x[i])
-            throw(SingularException(i))
-        end
-        k = trailing_zeros(x[i])
-        for j in 1:i-1
-            if isodd(x[j] >> k)
-                x[j] ⊻= x[i]
-                y[j] ⊻= y[i]
-            end
-        end
-        for j in i+1:lastindex(x)
-            if isodd(x[j] >> k)
-                x[j] ⊻= x[i]
-                y[j] ⊻= y[i]
-            end
-        end
-        p[k+1] = i
-    end
-    for i in eachindex(p)
-        z[i] = y[p[i]]
-    end
-end
-
-"""
-    z2number(::Number)
-    z2number(::AbstractMatrix{<:Number})
-
-Convert `Number` to `Bool` in the ``\\mathbb{Z}_2`` sense.
-
-# Examples
-
-```jldoctest
-julia> TR.z2number(25)
-true
-
-julia> TR.z2number(1:5)
-5-element BitVector:
- 1
- 0
- 1
- 0
- 1
-```
-"""
-z2number(x::Bool) = x
-z2number(x::Integer) = isodd(x)
-z2number(x::Number) = z2number(Integer(x))
-z2number(x::AbstractArray{Bool}) = x
-z2number(x::AbstractArray{<:Number}) = z2number.(x)
+include("z2helper.jl")
 
 """
     Z2RowMat{C<:Integer, R<:Integer}(data::Vector{R}, size::Int)
     Z2RowMat(undef, m, n)
     Z2RowMat(::AbstractMatrix)
 
-Z2 matrix stored in a `Vector` where each entry represents a row. `size` specifies the width. `C` and `R` are used for dispatch; they specify the integer type used to represent a column vector and a row vector respectively. See also [`bit2type`](@ref), [`Z2ColMat`](@ref)
+``\\mathbb{Z}_2`` matrix stored in a `Vector` where each entry represents a row. `size` specifies the width. `C` and `R` are used for dispatch; they specify the integer type used to represent a column vector and a row vector respectively. See also [`bit2type`](@ref), [`Z2ColMat`](@ref)
 
 # Example
 ```jldoctest
@@ -206,7 +31,7 @@ end
     Z2ColMat(undef, m, n)
     Z2ColMat(::AbstractMatrix)
 
-Z2 matrix stored in a `Vector` where each entry represents a column. `size` specifies the height. `C` and `R` are used for dispatch; they specify the integer type used to represent a column vector and a row vector respectively. See also [`bit2type`](@ref), [`Z2RowMat`](@ref)
+``\\mathbb{Z}_2`` matrix stored in a `Vector` where each entry represents a column. `size` specifies the height. `C` and `R` are used for dispatch; they specify the integer type used to represent a column vector and a row vector respectively. See also [`bit2type`](@ref), [`Z2RowMat`](@ref)
 
 # Example
 ```jldoctest
@@ -243,14 +68,14 @@ end
 size(A::Z2RowMat) = (length(A.data), A.size)
 size(A::Z2ColMat) = (A.size, length(A.data))
 
-getindex(A::Z2RowMat, i::Integer, j::Integer) = nthbit(A.data[i], j)
-getindex(A::Z2ColMat, i::Integer, j::Integer) = nthbit(A.data[j], i)
+getindex(A::Z2RowMat, i::Integer, j::Integer) = getbit(A.data[i], j-1)
+getindex(A::Z2ColMat, i::Integer, j::Integer) = getbit(A.data[j], i-1)
 
 function setindex!(A::Z2RowMat, x::Number, i::Integer, j::Integer)
-    A.data[i] = setnthbit(A.data[i], z2number(x), j)
+    A.data[i] = setbit(A.data[i], z2number(x), j-1)
 end
 function setindex!(A::Z2ColMat, x::Number, i::Integer, j::Integer)
-    A.data[j] = setnthbit(A.data[j], z2number(x), i)
+    A.data[j] = setbit(A.data[j], z2number(x), i-1)
 end
 
 for Typ in (Z2RowMat, Z2ColMat)
@@ -308,7 +133,7 @@ end
     Z2Vector{T<:Integer}(data::T, size::Int)
     Z2Vector(::AbstractVector)
 
-Z2 vector stored in an integer `data` where each bit represents an entry. `size` specifies the length.
+``\\mathbb{Z}_2`` vector stored in an integer `data` where each bit represents an entry. `size` specifies the length.
 
 # Example
 
@@ -339,9 +164,9 @@ end
 Z2Vector(v::AbstractVector) = Z2Vector(Bool.(mod.(v,2)))
 
 size(v::Z2Vector) = (v.size, )
-getindex(v::Z2Vector, i::Integer) = nthbit(v.data, i)
+getindex(v::Z2Vector, i::Integer) = getbit(v.data, i-1)
 function setindex!(v::Z2Vector, x::Number, i::Integer)
-    v.data = setnthbit(v.data, z2number(x), i)
+    v.data = setbit(v.data, z2number(x), i-1)
     v
 end
 
