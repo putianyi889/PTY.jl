bb35e225cee52e574a8c982b5c92dbdb564161d7
Tianyi Pu <912396513@qq.com>
2024-02-04 14:29:41 +0000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ DocMeta.setdocmeta!(PTY, :DocTestSetup, :(using PTY); recursive=true)
 	doctest(PTY)
 end
 @testset "Aqua" begin
-	Aqua.test_all(PTY, ambiguities = false, piracies = false, deps_compat = true, persistent_tests = false)
+	Aqua.test_all(PTY, ambiguities = false, piracies = false, deps_compat = true, persistent_tasks = false)
 end
 @testset "TR" begin
 	@testset "elementary logic" begin
